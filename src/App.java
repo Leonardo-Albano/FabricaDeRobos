36e039a396166800cad05cbfcccbbebac785c52d
Santiann <joaosantian@hotmail.com>
2024-06-22 18:36:44 -0300

    identação

--- src/App.java
+++ src/App.java
@@ -7,49 +7,42 @@ import Fabrica.*;
 
 public class App {
 
-    
-
     public static void main(String[] args) throws Exception {
-       
-        // teste();
         iniciar();
-        
-
-
     }
 
     private static Esteira instanciarEsteiras(
-                                    int idEsteira, 
-                                    int idInicialGrupo, List<String> tiposComponentes, int qtdGrupos,
-                                    int qtdFuncionarios, int idInicialFuncionarios
-                                    ){
+            int idEsteira,
+            int idInicialGrupo, List<String> tiposComponentes, int qtdGrupos,
+            int qtdFuncionarios, int idInicialFuncionarios) {
 
-                                        
         List<Thread> threads = new ArrayList<>();
         List<Grupo> listaGrupos = new ArrayList<>();
         Esteira esteira = new Esteira(idEsteira);
         for (int indexGrupo = 0; indexGrupo < qtdGrupos; indexGrupo++) {
 
-            int idGrupo = indexGrupo + idInicialGrupo; //Lógica para não criar ids duplicados
+            int idGrupo = indexGrupo + idInicialGrupo; // Lógica para não criar ids duplicados
             Grupo grupo = new Grupo(idGrupo, esteira);
 
             List<Semaphore> listaFerramentas = new ArrayList<>();
-            for (int indexFerramenta = 0; indexFerramenta < qtdFuncionarios; indexFerramenta++) {  
+            for (int indexFerramenta = 0; indexFerramenta < qtdFuncionarios; indexFerramenta++) {
                 listaFerramentas.add(new Semaphore(1));
             }
 
             List<Funcionario> listaFuncionarios = new ArrayList<>();
             String tipoComponente = tiposComponentes.get(indexGrupo);
-            for (int indexFuncionario = 0; indexFuncionario < qtdFuncionarios; indexFuncionario++) {  
+            for (int indexFuncionario = 0; indexFuncionario < qtdFuncionarios; indexFuncionario++) {
 
-                int idFuncionario = indexFuncionario + idInicialFuncionarios; //Lógica para não criar ids duplicados
-                int indexFerramentaEsq = indexFuncionario != qtdFuncionarios-1 ? indexFuncionario : qtdFuncionarios-1;
-                int indexFerramentaDir = indexFuncionario != qtdFuncionarios-1 ? indexFuncionario + 1 : 0;
+                int idFuncionario = indexFuncionario + idInicialFuncionarios; // Lógica para não criar ids duplicados
+                int indexFerramentaEsq = indexFuncionario != qtdFuncionarios - 1 ? indexFuncionario
+                        : qtdFuncionarios - 1;
+                int indexFerramentaDir = indexFuncionario != qtdFuncionarios - 1 ? indexFuncionario + 1 : 0;
 
                 Semaphore ferramentaEsq = listaFerramentas.get(indexFerramentaEsq);
                 Semaphore ferramentaDir = listaFerramentas.get(indexFerramentaDir);
 
-                Funcionario funcionario = new Funcionario(idFuncionario, grupo, tipoComponente, ferramentaEsq, ferramentaDir);
+                Funcionario funcionario = new Funcionario(idFuncionario, grupo, tipoComponente, ferramentaEsq,
+                        ferramentaDir);
 
                 threads.add(new Thread(funcionario));
                 listaFuncionarios.add(funcionario);
@@ -61,15 +54,15 @@ public class App {
         }
 
         esteira.setGrupos(listaGrupos);
-        
+
         for (Thread thread : threads) {
             thread.start();
         }
         return esteira;
     }
 
-    private static void iniciar() throws InterruptedException{
-        //Argumentos necessários para instanciar os objetos:
+    private static void iniciar() throws InterruptedException {
+        // Argumentos necessários para instanciar os objetos:
         int idEsteira;
         int idInicialGrupo;
         int idInicialFuncionarios;
@@ -80,10 +73,10 @@ public class App {
         // Instanciar a primeira esteira
         idEsteira = 1;
         idInicialGrupo = 1;
-        
+
         tiposComponentes = new ArrayList<>(
-            Arrays.asList(Componente.PERNA_ESQUERDA, Componente.PERNA_DIREITA, Componente.BRACO_ESQUERDO, Componente.BRACO_DIREITO)
-        );
+                Arrays.asList(Componente.PERNA_ESQUERDA, Componente.PERNA_DIREITA, Componente.BRACO_ESQUERDO,
+                        Componente.BRACO_DIREITO));
         qtdGrupos = tiposComponentes.size();
         qtdFuncionarios = 12;
         idInicialFuncionarios = 1;
@@ -98,25 +91,11 @@ public class App {
         qtdFuncionarios = 9;
 
         tiposComponentes = new ArrayList<>(
-            Arrays.asList(Componente.CARCACA, Componente.CABECA)
-        );
+                Arrays.asList(Componente.CARCACA, Componente.CABECA));
         qtdGrupos = tiposComponentes.size();
-        
-        Esteira esteira2 = instanciarEsteiras(idEsteira, idInicialGrupo, tiposComponentes, qtdGrupos, qtdFuncionarios, idInicialFuncionarios);
+
+        Esteira esteira2 = instanciarEsteiras(idEsteira, idInicialGrupo, tiposComponentes, qtdGrupos, qtdFuncionarios,
+                idInicialFuncionarios);
     }
 
-    private static void teste() throws InterruptedException {
-        // Argumentos para instanciar um teste 
-        int idEsteira = 1;
-        int idInicialGrupo = 1;
-        int idInicialFuncionarios = 1;
-
-        List<String> tiposComponentes = new ArrayList<>(
-                Arrays.asList(Componente.PERNA_ESQUERDA)
-        );
-        int qtdGrupos = 1;
-        int qtdFuncionarios = 2;
-
-        Esteira esteira = instanciarEsteiras(idEsteira, idInicialGrupo, tiposComponentes, qtdGrupos, qtdFuncionarios, idInicialFuncionarios);
-    }
 }
